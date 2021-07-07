eab43b73af98e4296c2934388f582a867c7f2532
Artur Arshakyan <artur@virtualpublicist.com>
2021-07-08 00:15:59 +0400

    Was created the first server

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-function sum(a, b){
-	return a + b;
-}
+const http = require('http')
 
-console.log(sum(10, 5))
+const server = http.createServer((req, res) => {
+    res.write('<h1>Hello Node JS</h1>')
+    res.end()
+})
+
+server.listen(3000, () => {
+    console.log('Server is running...')
+})
