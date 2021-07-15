7d46824987bd2825835fbb4c4f8997e7abf84f06
Artur Arshakyan <arturarshakyan5@gmail.com>
2021-07-16 01:10:15 +0400

    convert csv file to json file

--- index.js
+++ index.js
@@ -1,10 +1,52 @@
-const http = require('http')
+const fs = require('fs')
 
-const server = http.createServer((req, res) => {
-    res.write('<h1>Hello Node JS</h1>')
-    res.end()
-})
+try {
+    const csv = fs.readFileSync('addresses.csv')
+    const array = csv.toString().trim().split('\n')
 
-server.listen(3000, () => {
-    console.log('Server is running...')
-})
+    const result = []
+    const headers = array[0].split(',')
+
+    for (let i = 1; i < array.length; i++) {
+        const obj = {}
+        const str = array[i]
+
+        let newStr = ''
+        let flag = 0
+
+        for (let char of str) {
+            if (char === '"' && flag === 0) {
+                flag = 1
+            } else if (char === '"' && flag == 1) {
+                flag = 0
+            }
+
+            if (char === ',' && flag === 0) {
+                char = '|'
+            }
+
+            if (char !== '"') {
+                newStr += char
+            }
+        }
+
+        let properties = newStr.split('|')
+
+        for (let j in headers) {
+            if (properties[j].includes(',')) {
+                obj[headers[j]] = properties[j]
+                    .split(',')
+                    .map((item) => item.trim())
+            } else {
+                obj[headers[j]] = properties[j]
+            }
+        }
+
+        result.push(obj)
+    }
+
+    const json = JSON.stringify(result)
+    fs.writeFileSync('addresses.json', json)
+} catch (err) {
+    console.log(err)
+}
