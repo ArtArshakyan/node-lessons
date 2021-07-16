3b40cfeaae8df8d0bebb31cf37bc5c40d7315717
Artur Arshakyan <arturarshakyan5@gmail.com>
2021-07-16 13:26:12 +0400

    Unziped the folder, printed the directory structure

--- index.js
+++ index.js
@@ -1,10 +1,67 @@
-const http = require('http')
+const fs = require('fs')
+const path = require('path')
+const extract = require('extract-zip')
 
-const server = http.createServer((req, res) => {
-    res.write('<h1>Hello Node JS</h1>')
-    res.end()
-})
+async function main() {
+    try {
+        await extract('chalk.zip', { dir: path.join(__dirname) })
+        console.log('Extraction complete')
 
-server.listen(3000, () => {
-    console.log('Server is running...')
-})
+        let pathParams = process.argv[2] //Get the path parameter, the third parameter in the cmd command
+
+        if (!pathParams) {
+            pathParams = 'chalk'
+        }
+
+        //Turn to an absolute path
+        pathParams = path.resolve(pathParams)
+
+        //Level identification
+        let index = 0
+
+        function dirTree(pathParams) {
+            //Deep search
+
+            if (!fs.statSync(pathParams).isFile()) {
+                //Whether it is a file
+                console.log(markTree(index), getName(pathParams))
+
+                let dirLis = fs.readdirSync(pathParams)
+
+                index++ //Enter the next level
+
+                for (let i = 0; i < dirLis.length; i++) {
+                    dirTree(path.join(pathParams, dirLis[i]))
+                }
+
+                index-- //Back to previous
+            } else {
+                console.log(markTree(index), getName(pathParams))
+            }
+        }
+
+        //Generate proportional file breaks
+        function markTree(index) {
+            if (index === 0) {
+                return ''
+            }
+            let str = ''
+            for (let i = 0; i < index; i++) {
+                str += ' |__'
+            }
+            return str
+        }
+
+        //Return the directory name, or file name
+        function getName(pathParams) {
+            return path.parse(pathParams).base
+        }
+
+        //Specify the generated directory tree
+        dirTree(pathParams)
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+main()
