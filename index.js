a6c64abc93ba9beddf9fc54ee1d865e3c6ba7e80
Artur Arshakyan <arturarshakyan5@gmail.com>
2021-07-17 16:16:27 +0400

    Convert csv tojson with csvtojson module

--- index.js
+++ index.js
@@ -1,52 +1,75 @@
+const path = require('path')
 const fs = require('fs')
+const csvToJson = require('csvtojson')
 
-try {
-    const csv = fs.readFileSync('addresses.csv')
-    const array = csv.toString().trim().split('\n')
+const csvFilePath = path.join(__dirname, 'addresses.csv')
 
-    const result = []
-    const headers = array[0].split(',')
+csvToJson()
+    .fromFile(csvFilePath)
+    .then((jsonObj) => {
+        
+        fs.writeFileSync(
+            path.join(__dirname, 'addresses.json'),
+            JSON.stringify(jsonObj, null, 2)
+        )
 
-    for (let i = 1; i < array.length; i++) {
-        const obj = {}
-        const str = array[i]
+        // fs.writeFile(
+        //     path.join(__dirname, 'addresses.json'),
+        //     JSON.stringify(jsonObj, null, 2),
+        //     (err) => {
+        //         if (err) console.log(err)
+        //     }
+        // )
+    })
 
-        let newStr = ''
-        let flag = 0
+/** Custom Implementation */
+// try {
+//     const csv = fs.readFileSync('addresses.csv')
+//     const array = csv.toString().trim().split('\n')
 
-        for (let char of str) {
-            if (char === '"' && flag === 0) {
-                flag = 1
-            } else if (char === '"' && flag == 1) {
-                flag = 0
-            }
+//     const result = []
+//     const headers = array[0].split(',')
 
-            if (char === ',' && flag === 0) {
-                char = '|'
-            }
+//     for (let i = 1; i < array.length; i++) {
+//         const obj = {}
+//         const str = array[i]
 
-            if (char !== '"') {
-                newStr += char
-            }
-        }
+//         let newStr = ''
+//         let flag = 0
 
-        let properties = newStr.split('|')
+//         for (let char of str) {
+//             if (char === '"' && flag === 0) {
+//                 flag = 1
+//             } else if (char === '"' && flag == 1) {
+//                 flag = 0
+//             }
 
-        for (let j in headers) {
-            if (properties[j].includes(',')) {
-                obj[headers[j]] = properties[j]
-                    .split(',')
-                    .map((item) => item.trim())
-            } else {
-                obj[headers[j]] = properties[j]
-            }
-        }
+//             if (char === ',' && flag === 0) {
+//                 char = '|'
+//             }
 
-        result.push(obj)
-    }
+//             if (char !== '"') {
+//                 newStr += char
+//             }
+//         }
 
-    const json = JSON.stringify(result)
-    fs.writeFileSync('addresses.json', json)
-} catch (err) {
-    console.log(err)
-}
+//         let properties = newStr.split('|')
+
+//         for (let j in headers) {
+//             if (properties[j].includes(',')) {
+//                 obj[headers[j]] = properties[j]
+//                     .split(',')
+//                     .map((item) => item.trim())
+//             } else {
+//                 obj[headers[j]] = properties[j]
+//             }
+//         }
+
+//         result.push(obj)
+//     }
+
+//     const json = JSON.stringify(result)
+//     fs.writeFileSync('addresses.json', json)
+// } catch (err) {
+//     console.log(err)
+// }
