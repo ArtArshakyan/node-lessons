45f72b2927b44bae5efcf690da77ee769959688c
Artur Arshakyan <arturarshakyan5@gmail.com>
2021-07-20 22:41:36 +0400

    rate server

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
+const http = require('http')
 const cluster = require('cluster')
+
+const port = 3000
 
 if (cluster.isMaster) {
 
-    // Fork workers.
+    // Fork workers
     for (let i = 1; i <= 2; i++) { // for 2 processor
         cluster.fork()
     }
     
 } else {
     console.log(factorial(50))
-    process.exit(0)
+
+    http.createServer((req, res) => {
+        res.writeHead(200)
+        res.end(`Current process ${process.pid}`)
+    }).listen(port, () => {
+        console.log(`Server is running on port ${port}`)
+    })
 }
 
 function factorial(n) {
