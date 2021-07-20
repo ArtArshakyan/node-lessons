e79d3bd8525899848f64eaf1a9e318d6114f5655
Artur Arshakyan <arturarshakyan5@gmail.com>
2021-07-21 01:09:47 +0400

    send message from worker to main

--- index.js
+++ index.js
@@ -3,28 +3,39 @@ const cluster = require('cluster')
 
 const port = 3000
 
-if (cluster.isMaster) {
-
-    // Fork workers
-    for (let i = 1; i <= 2; i++) { // for 2 processor
-        cluster.fork()
-    }
-    
-} else {
-    console.log(factorial(50))
-
-    http.createServer((req, res) => {
-        res.writeHead(200)
-        res.end(`Current process ${process.pid}`)
-    }).listen(port, () => {
-        console.log(`Server is running on port ${port}`)
-    })
-}
-
 function factorial(n) {
     if (n <= 1) {
         return 1
     }
 
     return n * factorial(n - 1)
+}
+
+if (cluster.isMaster) {
+
+    let sum = 0;
+    for (let i = 0; i < 2; i++) {
+        const worker = cluster.fork()
+
+        worker.on('message', (msg) => {
+            console.log('Master ' + process.pid + ' received message from worker ',msg)
+
+            sum += msg
+
+            console.log('sum', sum)
+        })
+    }
+
+
+
+} else {
+    console.log('Worker ' + process.pid + ' has started.')
+
+    http.createServer((req, res) => {
+        res.writeHead(200)
+        res.end(`Current process ${process.pid}`)
+        process.send(factorial(25))
+    }).listen(port, () => {
+        console.log(`Server is running on port ${port}`)
+    })
 }
