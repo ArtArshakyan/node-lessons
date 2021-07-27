dd727b72c8dd08f95f5a487080bb72005553d6df
Artur Arshakyan <arturarshakyan5@gmail.com>
2021-07-27 11:37:35 +0400

    coding with streams

--- index.js
+++ index.js
@@ -1,10 +1,41 @@
-const http = require('http')
+const fs = require('fs')
+const path = require('path')
+const { createGzip } = require('zlib')
 
-const server = http.createServer((req, res) => {
-    res.write('<h1>Hello Node JS</h1>')
-    res.end()
-})
+function createFolder(cb) {
+    const newFolder = path.join(__dirname, 'new-folder')
 
-server.listen(3000, () => {
-    console.log('Server is running...')
-})
+    fs.mkdir(newFolder, (err) => {
+        if (err) {
+            return console.log(err)
+        }
+
+        console.log('Folder created successfully!!!')
+
+        cb(newFolder)
+    })
+}
+
+async function zipFiles(newFolder) {
+    try {
+        const myFolder = path.join(__dirname, 'test-folder')
+        const folderItems = await fs.readdirSync(myFolder)
+
+        folderItems.map((item) => {
+            const readFile = fs.createReadStream(`${myFolder}/${item}`)
+            const writeFile = fs.createWriteStream(`${newFolder}/${item}.gz`)
+
+            return readFile.pipe(createGzip()).pipe(writeFile).on('finish', (err) => {
+                if(err) {
+                    return console.log(err)
+                }
+            })
+        })
+
+        console.log('Files zipped successfully!!!')
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+createFolder(zipFiles)
