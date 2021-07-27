f7afe9c630e6819a43c677ac65cba6650919d3f4
Artur Arshakyan <arturarshakyan5@gmail.com>
2021-07-27 11:37:35 +0400

    coding with streams

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
-const http = require('http')
+const fs = require('fs')
+const path = require('path')
+const zlib = require('zlib').createGzip()
 
-const server = http.createServer((req, res) => {
-    res.write('<h1>Hello Node JS</h1>')
-    res.end()
-})
+const myFolder = path.join(__dirname, 'test-folder')
+const newFolder = path.join(__dirname, 'new-folder')
 
-server.listen(3000, () => {
-    console.log('Server is running...')
+fs.mkdir(newFolder, (err) => {
+    if (err) {
+        return console.log(err)
+    }
+
+    const folderItems = fs.readdirSync(myFolder)
+
+    folderItems.map((item) => {
+        const readFile = fs.createReadStream(`${myFolder}/${item}`)
+        const writeFile = fs.createWriteStream(`${newFolder}/${item}.gz`)
+
+        return readFile.pipe(zlib).pipe(writeFile).on('finish', (err) => {
+            if(err) {
+                return console.log(err)
+            }
+        })
+    })
 })
