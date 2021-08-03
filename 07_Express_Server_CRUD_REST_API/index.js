7aeee53b034e1ac8a65749c7cdd4a6ee77156dd8
Artur Arshakyan <arturarshakyan5@gmail.com>
2021-08-03 22:03:38 +0400

    Initiliaze docker, create docker containers

--- 07_Express_Server_CRUD_REST_API/index.js
+++ 07_Express_Server_CRUD_REST_API/index.js
@@ -2,13 +2,13 @@ import express from 'express'
 import usersRoutes from './routes/users.js'
 
 const app = express()
-const PORT = 9001
+const PORT = 8000
 
 app.use(express.json())
 app.use('/users', usersRoutes)
 
 app.get('/', (req, res) => {
-    res.send('Hello Express!!!')
+    res.send('Hello REST API!')
 })
 
 app.listen(PORT, () => {
